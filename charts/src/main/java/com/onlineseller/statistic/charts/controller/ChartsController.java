5a104f22397a61af4c8a7cf14d5fa59514c2cb77
gaoxiang15125 <630268696@qq.com>
2019-04-22 18:35:09 +0800

    添加第三个模块

---
+++ charts/src/main/java/com/onlineseller/statistic/charts/controller/ChartsController.java
@@ -0,0 +1,11 @@
+package com.onlineseller.statistic.charts.controller;
+
+/**
+ * @program: demo
+ * @Description:
+ * @author: Mr.gao
+ * @create: 2019-04-22 14:10
+ * @email: 630268696@qq.com
+ **/
+public class ChartsController {
+}
